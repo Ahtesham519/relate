4b4ec366bd72cd2f7eb82237441be51ea1247314
Ahtesham Ul Haq(@hptouchpad519) <hptouchpad519@gmail.com>
2017-02-09 17:14:34 +0500

    Create relate.cpp

---
+++ relate.cpp
@@ -0,0 +1,19 @@
+//relate.cpp
+//demostrates relational operators
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int numb ;
+
+cout<<"Enter a number:  ";
+cin>> numb;
+
+cout<<"numb<10  is  " << (numb < 10 )  << endl;
+cout<< "numb>10 is " << (numb >10 ) <<endl;
+cout << "numb == 10  is " << (numb == 10) << endl;
+
+return 0 ;
+}
